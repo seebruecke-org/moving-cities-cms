0d33583c540603286eb4fc938122574c220b682a
Gustav Pursche <pursche@posteo.de>
2021-01-26 15:01:00 +0100

    Remove STRAPI_PUBLIC_URL

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
-  url: env('STRAPI_PUBLIC_URL', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bd56bbd9f4023bc6860b069f9005e90b'),
